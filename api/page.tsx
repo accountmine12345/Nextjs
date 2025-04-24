d6edf70877bd5d752fa12f2fb1141be71adb95d5
accountmine12345 <k.karmacharya@boxhill.edu.au>
2025-04-24 10:44:40 +1000

    Add files via upload

--- api/page.tsx
+++ api/page.tsx
@@ -1,6 +1,7 @@
 // app/courses/page.tsx
+import React from 'react';
 export default async function CoursesPage() {
-    const res = await fetch('https://api.example.com/courses');
+    const res = await fetch('https://cid-api.com/courses');
     const courses = await res.json();
 
     return (
@@ -9,8 +10,28 @@ export default async function CoursesPage() {
             <ul>
                 {courses.map((course: { id: number; title: string, duration: string, description: string }) => (
                     <li key={course.id}>{course.title}</li>
+                    <li>{course.duration}</li>
+                    <li>course.description</li>
+
                 ))}
             </ul>
         </div>
     );
+}
+
+//view More 
+export default function ViewMore({ course, onClick }) {
+
+    return (
+        <div className="flex flex-col border p-4 rounded-lg hover:shadow-lg 
+       transition-shadow bg-white" >
+            <Link href={`/course/${course.id}`}>
+                <Button
+                    label="View More"
+                    className="bg-indigo-500 hover:bg-gray-600 focus-visible:outline- 
+          indigo-600 w-32 flex flex-row justify-center items-center"
+                />
+            </Link>
+        </div>
+    )
 }
